858fb72e2a8698296a4dac2d494c58a47ee7bd75
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-10-13 16:36:10 +0800

    Bugfix.

--- bin/mask.py
+++ bin/mask.py
@@ -16,11 +16,11 @@ def main():
         empty = 0
     else:
         empty = (0, 0, 0, 0)
-    for subfigure in range(img.size[0] // 264):
+    for x in range(256):
         for y in range(64):
-            for x in range(256):
-                if (y - args.delta_y) * 2 + min(x, 255 - x) < 128:
-                    img.putpixel((x, 310 - 1 - y), empty)
+            if (y - args.delta_y) * 2 + min(x, 255 - x) < 128:
+                for subfigure in range(img.size[0] // 264):
+                    img.putpixel((subfigure * 264 + x, 310 - 1 - y), empty)
     img.save(args.output_path)
 
 if __name__ == "__main__":
